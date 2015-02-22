49301149583766d7e5c9f66986675c6a204c2792
Bruce E. Pascoe <fatcerberus1@gmail.com>
2015-02-21 23:39:05 -0500

    Finish up ByteArray implementation
    These are true byte arrays under the hood.  Thank goodness for Duktape
    implementing the ES6 Proxy object or this wouldn't have been possible!

--- minisphere/bytearray.c
+++ minisphere/bytearray.c
@@ -8,6 +8,8 @@ static duk_ret_t _js_CreateByteArrayFromString(duk_context* ctx);
 static duk_ret_t _js_CreateStringFromByteArray(duk_context* ctx);
 static duk_ret_t _js_HashByteArray(duk_context* ctx);
 static duk_ret_t _js_ByteArray_finalize(duk_context* ctx);
+static duk_ret_t _js_ByteArray_getProp(duk_context* ctx);
+static duk_ret_t _js_ByteArray_setProp(duk_context* ctx);
 static duk_ret_t _js_ByteArray_concat(duk_context* ctx);
 static duk_ret_t _js_ByteArray_slice(duk_context* ctx);
 
@@ -29,6 +31,16 @@ duk_push_sphere_bytearray(duk_context* ctx, uint8_t* buffer, size_t size)
 	duk_push_c_function(ctx, _js_ByteArray_finalize, DUK_VARARGS); duk_set_finalizer(ctx, -2);
 	duk_push_c_function(ctx, _js_ByteArray_concat, DUK_VARARGS); duk_put_prop_string(ctx, -2, "concat");
 	duk_push_c_function(ctx, _js_ByteArray_slice, DUK_VARARGS); duk_put_prop_string(ctx, -2, "slice");
+	
+	duk_push_global_object(ctx);
+	duk_get_prop_string(ctx, -1, "Proxy");
+	duk_dup(ctx, -3);
+	duk_push_object(ctx);
+	duk_push_c_function(ctx, _js_ByteArray_getProp, DUK_VARARGS); duk_put_prop_string(ctx, -2, "get");
+	duk_push_c_function(ctx, _js_ByteArray_setProp, DUK_VARARGS); duk_put_prop_string(ctx, -2, "set");
+	duk_new(ctx, 2);
+	duk_remove(ctx, -2);
+	duk_remove(ctx, -2);
 }
 
 static duk_ret_t
@@ -86,6 +98,42 @@ _js_ByteArray_finalize(duk_context* ctx)
 	duk_get_prop_string(ctx, 0, "\xFF" "buffer"); buffer = duk_get_pointer(ctx, -1); duk_pop(ctx);
 	free(buffer);
 	return 0;
+}
+
+static duk_ret_t
+_js_ByteArray_getProp(duk_context* ctx)
+{
+	uint8_t*     buffer;
+	unsigned int offset;
+	size_t       size;
+
+	duk_get_prop_string(ctx, 0, "\xFF" "buffer"); buffer = duk_get_pointer(ctx, -1); duk_pop(ctx);
+	duk_get_prop_string(ctx, 0, "\xFF" "size"); size = duk_get_uint(ctx, -1); duk_pop(ctx);
+	if ((offset = duk_require_uint(ctx, 1)) < size) {
+		duk_push_uint(ctx, buffer[offset]);
+		return 1;
+	}
+	else {
+		duk_error(ctx, DUK_ERR_RANGE_ERROR, "ByteArray[]: Byte offset (%u) is outside of the buffer", offset);
+	}
+}
+
+static duk_ret_t
+_js_ByteArray_setProp(duk_context* ctx)
+{
+	uint8_t*     buffer;
+	unsigned int offset;
+	size_t       size;
+
+	duk_get_prop_string(ctx, 0, "\xFF" "buffer"); buffer = duk_get_pointer(ctx, -1); duk_pop(ctx);
+	duk_get_prop_string(ctx, 0, "\xFF" "size"); size = duk_get_uint(ctx, -1); duk_pop(ctx);
+	if ((offset = duk_require_uint(ctx, 1)) < size) {
+		buffer[offset] = duk_get_uint(ctx, 2);
+		return 0;
+	}
+	else {
+		duk_error(ctx, DUK_ERR_RANGE_ERROR, "ByteArray[]: Byte offset (%u) is outside of the buffer", offset);
+	}
 }
 
 static duk_ret_t
