bcecb2305009f8dfe4847b6f90b4bcac25bec54e
Bruce E. Pascoe <fatcerberus1@gmail.com>
2015-03-22 00:54:08 -0400

    Refactor windowstyle.c
    Holy crap, is the early minisphere code a mess!  Honestly the low number
    of regressions so far is a wonder in itself: The whole engine was put
    together in such a hurry that it really should have more bugs than it
    does.  Oh well, I'm not complaining! :-)

--- minisphere/windowstyle.c
+++ minisphere/windowstyle.c
@@ -1,22 +1,16 @@
 #include "minisphere.h"
 #include "api.h"
 #include "color.h"
+#include "image.h"
 
 #include "windowstyle.h"
 
-struct windowstyle
-{
-	int             bg_style;
-	ALLEGRO_BITMAP* bitmaps[9];
-};
-
-static void            _duk_push_winstyle          (duk_context* ctx, windowstyle_t* winstyle);
-static duk_ret_t       js_GetSystemWindowStyle     (duk_context* ctx);
-static duk_ret_t       js_LoadWindowStyle          (duk_context* ctx);
-static duk_ret_t       js_WindowStyle_finalize     (duk_context* ctx);
-static duk_ret_t       js_WindowStyle_toString     (duk_context* ctx);
-static duk_ret_t       js_WindowStyle_drawWindow   (duk_context* ctx);
-static duk_ret_t       js_WindowStyle_setColorMask (duk_context* ctx);
+static duk_ret_t js_GetSystemWindowStyle(duk_context* ctx);
+static duk_ret_t js_LoadWindowStyle          (duk_context* ctx);
+static duk_ret_t js_WindowStyle_finalize     (duk_context* ctx);
+static duk_ret_t js_WindowStyle_toString     (duk_context* ctx);
+static duk_ret_t js_WindowStyle_drawWindow   (duk_context* ctx);
+static duk_ret_t js_WindowStyle_setColorMask (duk_context* ctx);
 
 static windowstyle_t* s_sys_winstyle = NULL;
 
@@ -29,6 +23,12 @@ enum wstyle_bg_type
 	WSTYLE_BG_STRETCH_GRADIENT
 };
 
+struct windowstyle
+{
+	int      bg_style;
+	image_t* images[9];
+};
+
 #pragma pack(push, 1)
 struct rws_rgba
 {
@@ -39,7 +39,7 @@ struct rws_header
 {
 	uint8_t         signature[4];
 	int16_t         version;
-	uint8_t         edge_width;
+	uint8_t         edge_w_h;
 	uint8_t         background_mode;
 	struct rws_rgba corner_colors[4];
 	uint8_t         edge_offsets[4];
@@ -50,12 +50,14 @@ struct rws_header
 windowstyle_t*
 load_windowstyle(const char* path)
 {
-	ALLEGRO_BITMAP*     bitmap;
-	ALLEGRO_FILE*       file;
-	struct rws_header   rws;
-	int16_t             w, h;
-	windowstyle_t*      winstyle = NULL;
-	int                 i;
+	image_t*          atlas = NULL;
+	ALLEGRO_FILE*     file;
+	image_t*          image;
+	int16_t           max_w = 0, max_h = 0;
+	struct rws_header rws;
+	int16_t           w, h;
+	windowstyle_t*    winstyle = NULL;
+	int               i;
 
 	if ((file = al_fopen(path, "rb")) == NULL) goto on_error;
 	if ((winstyle = calloc(1, sizeof(windowstyle_t))) == NULL) goto on_error;
@@ -64,19 +66,22 @@ load_windowstyle(const char* path)
 	if (memcmp(rws.signature, ".rws", 4) != 0) goto on_error;
 	switch (rws.version) {
 	case 1:
+		max_w = rws.edge_w_h; max_h = rws.edge_w_h;
+		if (!(atlas = create_image(rws.edge_w_h * 3, rws.edge_w_h * 3)))
+			goto on_error;
 		for (i = 0; i < 9; ++i) {
-			if ((bitmap = al_fread_bitmap(file, rws.edge_width, rws.edge_width)) == NULL)
+			if (!(image = read_image(file, rws.edge_w_h, rws.edge_w_h)))
 				goto on_error;
-			winstyle->bitmaps[i] = bitmap;
+			winstyle->images[i] = image;
 		}
 		break;
 	case 2:
 		for (i = 0; i < 9; ++i) {
 			if (al_fread(file, &w, 2) != 2 || al_fread(file, &h, 2) != 2)
 				goto on_error;
-			if ((bitmap = al_fread_bitmap(file, w, h)) == NULL)
+			if ((image = read_image(file, w, h)) == NULL)
 				goto on_error;
-			winstyle->bitmaps[i] = bitmap;
+			winstyle->images[i] = image;
 		}
 		break;
 	default:  // invalid version number
@@ -84,16 +89,17 @@ load_windowstyle(const char* path)
 	}
 	al_fclose(file);
 	winstyle->bg_style = rws.background_mode;
+	free_image(atlas);
 	return winstyle;
 
 on_error:
 	if (file != NULL) al_fclose(file);
 	if (winstyle != NULL) {
-		for (i = 0; i < 9; ++i) {
-			if (winstyle->bitmaps[i] != NULL) al_destroy_bitmap(winstyle->bitmaps[i]);
-		}
+		for (i = 0; i < 9; ++i)
+			free_image(winstyle->images[i]);
 		free(winstyle);
 	}
+	free_image(atlas);
 	return NULL;
 }
 
@@ -103,16 +109,17 @@ free_windowstyle(windowstyle_t* winstyle)
 	int i;
 
 	for (i = 0; i < 9; ++i) {
-		al_destroy_bitmap(winstyle->bitmaps[i]);
+		free_image(winstyle->images[i]);
 	}
-	al_free(winstyle);
+	free(winstyle);
 }
 
 void
 draw_window(windowstyle_t* winstyle, color_t mask, int x, int y, int width, int height)
 {
-	ALLEGRO_COLOR native_mask = to_native_color(mask);
-	int           w[9], h[9];
+	ALLEGRO_BITMAP* bitmaps[9];
+	ALLEGRO_COLOR   native_mask = to_native_color(mask);
+	int             w[9], h[9];
 	
 	int i;
 	
@@ -127,26 +134,27 @@ draw_window(windowstyle_t* winstyle, color_t mask, int x, int y, int width, int 
 	// 8 - background
 
 	for (i = 0; i < 9; ++i) {
-		w[i] = al_get_bitmap_width(winstyle->bitmaps[i]);
-		h[i] = al_get_bitmap_height(winstyle->bitmaps[i]);
+		bitmaps[i] = get_image_bitmap(winstyle->images[i]);
+		w[i] = get_image_width(winstyle->images[i]);
+		h[i] = get_image_height(winstyle->images[i]);
 	}
 	
 	switch (winstyle->bg_style) {
 	case WSTYLE_BG_TILE:
-		al_draw_tinted_tiled_bitmap(winstyle->bitmaps[8], native_mask, x, y, width, height);
+		al_draw_tinted_tiled_bitmap(bitmaps[8], native_mask, x, y, width, height);
 		break;
 	case WSTYLE_BG_STRETCH:
-		al_draw_tinted_scaled_bitmap(winstyle->bitmaps[8], native_mask, 0, 0, w[8], h[8], x, y, width, height, 0x0);
+		al_draw_tinted_scaled_bitmap(bitmaps[8], native_mask, 0, 0, w[8], h[8], x, y, width, height, 0x0);
 		break;
 	}
-	al_draw_tinted_bitmap(winstyle->bitmaps[0], native_mask, x - w[0], y - h[0], 0x0);
-	al_draw_tinted_bitmap(winstyle->bitmaps[2], native_mask, x + width, y - h[2], 0x0);
-	al_draw_tinted_bitmap(winstyle->bitmaps[4], native_mask, x + width, y + height, 0x0);
-	al_draw_tinted_bitmap(winstyle->bitmaps[6], native_mask, x - w[6], y + height, 0x0);
-	al_draw_tinted_tiled_bitmap(winstyle->bitmaps[1], native_mask, x, y - h[1], width, h[1]);
-	al_draw_tinted_tiled_bitmap(winstyle->bitmaps[3], native_mask, x + width, y, w[3], height);
-	al_draw_tinted_tiled_bitmap(winstyle->bitmaps[5], native_mask, x, y + height, width, h[5]);
-	al_draw_tinted_tiled_bitmap(winstyle->bitmaps[7], native_mask, x - w[7], y, w[7], height);
+	al_draw_tinted_bitmap(bitmaps[0], native_mask, x - w[0], y - h[0], 0x0);
+	al_draw_tinted_bitmap(bitmaps[2], native_mask, x + width, y - h[2], 0x0);
+	al_draw_tinted_bitmap(bitmaps[4], native_mask, x + width, y + height, 0x0);
+	al_draw_tinted_bitmap(bitmaps[6], native_mask, x - w[6], y + height, 0x0);
+	al_draw_tinted_tiled_bitmap(bitmaps[1], native_mask, x, y - h[1], width, h[1]);
+	al_draw_tinted_tiled_bitmap(bitmaps[3], native_mask, x + width, y, w[3], height);
+	al_draw_tinted_tiled_bitmap(bitmaps[5], native_mask, x, y + height, width, h[5]);
+	al_draw_tinted_tiled_bitmap(bitmaps[7], native_mask, x - w[7], y, w[7], height);
 }
 
 void
@@ -169,7 +177,7 @@ init_windowstyle_api(void)
 }
 
 static void
-_duk_push_winstyle(duk_context* ctx, windowstyle_t* winstyle)
+duk_push_sphere_windowstyle(duk_context* ctx, windowstyle_t* winstyle)
 {
 	duk_push_object(ctx);
 	duk_push_pointer(ctx, winstyle); duk_put_prop_string(ctx, -2, "\xFF" "ptr");
@@ -183,28 +191,25 @@ _duk_push_winstyle(duk_context* ctx, windowstyle_t* winstyle)
 static duk_ret_t
 js_GetSystemWindowStyle(duk_context* ctx)
 {
-	if (s_sys_winstyle != NULL) {
-		_duk_push_winstyle(ctx, s_sys_winstyle);
-		return 1;
-	}
-	else {
-		duk_error(ctx, DUK_ERR_ERROR, "GetSystemWindowStyle(): No system window style available");
-	}
+	if (s_sys_winstyle == NULL)
+		js_error(JS_REF_ERROR, -1, "GetSystemWindowStyle(): No system window style available");
+	duk_push_sphere_windowstyle(ctx, s_sys_winstyle);
+	return 1;
 }
 
 static duk_ret_t
 js_LoadWindowStyle(duk_context* ctx)
 {
-	const char*    filename = NULL;
+	const char* filename = duk_require_string(ctx, 0);
+
 	char*          path = NULL;
 	windowstyle_t* winstyle = NULL;
 
-	filename = duk_require_string(ctx, 0);
 	path = get_asset_path(filename, "windowstyles", false);
-	if ((winstyle = load_windowstyle(path)) == NULL)
-		duk_error(ctx, DUK_ERR_ERROR, "LoadWindowStyle(): Unable to load windowstyle file '%s'", filename);
+	if (!(winstyle = load_windowstyle(path)))
+		js_error(JS_ERROR, -1, "LoadWindowStyle(): Failed to load windowstyle file '%s'", filename);
 	free(path);
-	_duk_push_winstyle(ctx, winstyle);
+	duk_push_sphere_windowstyle(ctx, winstyle);
 	return 1;
 }
 
@@ -214,7 +219,7 @@ js_WindowStyle_finalize(duk_context* ctx)
 	windowstyle_t* winstyle;
 
 	duk_get_prop_string(ctx, 0, "\xFF" "ptr"); winstyle = duk_get_pointer(ctx, -1); duk_pop(ctx);
-	//free_windowstyle(winstyle);
+	free_windowstyle(winstyle);
 	return 0;
 }
 
@@ -228,19 +233,19 @@ js_WindowStyle_toString(duk_context* ctx)
 static duk_ret_t
 js_WindowStyle_drawWindow(duk_context* ctx)
 {
-	color_t        color_mask;
+	int x = duk_require_int(ctx, 0);
+	int y = duk_require_int(ctx, 1);
+	int w = duk_require_int(ctx, 2);
+	int h = duk_require_int(ctx, 3);
+	
+	color_t        mask;
 	windowstyle_t* winstyle;
-	int            x, y, w, h;
 
-	x = duk_to_int(ctx, 0);
-	y = duk_to_int(ctx, 1);
-	w = duk_to_int(ctx, 2);
-	h = duk_to_int(ctx, 3);
 	duk_push_this(ctx);
 	duk_get_prop_string(ctx, -1, "\xFF" "ptr"); winstyle = duk_get_pointer(ctx, -1); duk_pop(ctx);
-	duk_get_prop_string(ctx, -1, "\xFF" "color_mask"); color_mask = duk_require_sphere_color(ctx, -1); duk_pop(ctx);
+	duk_get_prop_string(ctx, -1, "\xFF" "color_mask"); mask = duk_require_sphere_color(ctx, -1); duk_pop(ctx);
 	duk_pop(ctx);
-	draw_window(winstyle, color_mask, x, y, w, h);
+	draw_window(winstyle, mask, x, y, w, h);
 	return 0;
 }
 
--- minisphere/windowstyle.h
+++ minisphere/windowstyle.h
@@ -2,7 +2,9 @@
 
 typedef struct windowstyle windowstyle_t;
 
-extern windowstyle_t* load_windowstyle     (const char* path);
-extern void           free_windowstyle     (windowstyle_t* windowstyle);
-extern void           init_windowstyle_api (void);
-extern void           draw_window          (windowstyle_t* winstyle, color_t mask, int x, int y, int width, int height);
+extern windowstyle_t* load_windowstyle (const char* path);
+extern void           free_windowstyle (windowstyle_t* windowstyle);
+extern void           draw_window      (windowstyle_t* winstyle, color_t mask, int x, int y, int width, int height);
+
+extern void init_windowstyle_api        (void);
+extern void duk_push_sphere_windowstyle (duk_context* ctx, windowstyle_t* winstyle);
