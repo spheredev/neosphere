f4d97f73db1521fb0817a4b66d4f474da8e08b9a
Bruce Pascoe <fatcerberus@icloud.com>
2015-04-20 00:42:22 -0400

    Really, REALLY fix the sound deadlocks this time
    The lockup was originally "fixed" by draining the stream before
    reloading it.  That worked, but caused delays when playing the same
    sound in rapid succession.  This is a proper fix, which involved edits
    to Allegro itself.

--- minisphere/sound.c
+++ minisphere/sound.c
@@ -171,7 +171,6 @@ reload_sound(sound_t* sound)
 	if (!(new_stream = al_load_audio_stream(sound->path, 4, 1024)))
 		return false;
 	if (sound->stream != NULL) {
-		al_drain_audio_stream(sound->stream);
 		al_destroy_audio_stream(sound->stream);
 	}
 	sound->stream = new_stream;
