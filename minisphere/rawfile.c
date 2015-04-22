480b10214b71d0bb307b971edbfcda61897024e9
Bruce Pascoe <fatcerberus@icloud.com>
2015-04-22 11:38:24 -0400

    Add RawFile constructor and properties
    This is the last one.  Every single Sphere object has a constructor!
    :dancer:  Now, time for some testing...

--- minisphere/rawfile.c
+++ minisphere/rawfile.c
@@ -4,24 +4,39 @@
 
 #include "rawfile.h"
 
-static duk_ret_t js_HashRawFile         (duk_context* ctx);
-static duk_ret_t js_OpenRawFile         (duk_context* ctx);
-static duk_ret_t js_RawFile_finalize    (duk_context* ctx);
-static duk_ret_t js_RawFile_toString    (duk_context* ctx);
-static duk_ret_t js_RawFile_getPosition (duk_context* ctx);
-static duk_ret_t js_RawFile_getSize     (duk_context* ctx);
-static duk_ret_t js_RawFile_setPosition (duk_context* ctx);
-static duk_ret_t js_RawFile_close       (duk_context* ctx);
-static duk_ret_t js_RawFile_read        (duk_context* ctx);
-static duk_ret_t js_RawFile_write       (duk_context* ctx);
+static duk_ret_t js_HashRawFile          (duk_context* ctx);
+static duk_ret_t js_OpenRawFile          (duk_context* ctx);
+static duk_ret_t js_new_RawFile          (duk_context* ctx);
+static duk_ret_t js_RawFile_finalize     (duk_context* ctx);
+static duk_ret_t js_RawFile_toString     (duk_context* ctx);
+static duk_ret_t js_RawFile_get_position (duk_context* ctx);
+static duk_ret_t js_RawFile_set_position (duk_context* ctx);
+static duk_ret_t js_RawFile_get_size     (duk_context* ctx);
+static duk_ret_t js_RawFile_close        (duk_context* ctx);
+static duk_ret_t js_RawFile_read         (duk_context* ctx);
+static duk_ret_t js_RawFile_readString   (duk_context* ctx);
+static duk_ret_t js_RawFile_write        (duk_context* ctx);
 
 void
 init_rawfile_api(void)
 {
-	duk_push_global_object(g_duktape);
-	duk_push_c_function(g_duktape, js_HashRawFile, DUK_VARARGS); duk_put_prop_string(g_duktape, -2, "HashRawFile");
-	duk_push_c_function(g_duktape, js_OpenRawFile, DUK_VARARGS); duk_put_prop_string(g_duktape, -2, "OpenRawFile");
-	duk_pop(g_duktape);
+	// RawFile API functions
+	register_api_func(g_duktape, NULL, "HashRawFile", js_HashRawFile);
+	
+	// RawFile object
+	register_api_func(g_duktape, NULL, "OpenRawFile", js_OpenRawFile);
+	register_api_ctor(g_duktape, "RawFile", js_new_RawFile, js_RawFile_finalize);
+	register_api_func(g_duktape, "RawFile", "toString", js_RawFile_toString);
+	register_api_prop(g_duktape, "RawFile", "length", js_RawFile_get_size, NULL);
+	register_api_prop(g_duktape, "RawFile", "position", js_RawFile_get_position, js_RawFile_set_position);
+	register_api_prop(g_duktape, "RawFile", "size", js_RawFile_get_size, NULL);
+	register_api_func(g_duktape, "RawFile", "getPosition", js_RawFile_get_position);
+	register_api_func(g_duktape, "RawFile", "setPosition", js_RawFile_set_position);
+	register_api_func(g_duktape, "RawFile", "getSize", js_RawFile_get_size);
+	register_api_func(g_duktape, "RawFile", "close", js_RawFile_close);
+	register_api_func(g_duktape, "RawFile", "read", js_RawFile_read);
+	register_api_func(g_duktape, "RawFile", "readString", js_RawFile_readString);
+	register_api_func(g_duktape, "RawFile", "write", js_RawFile_write);
 }
 
 static duk_ret_t
@@ -46,6 +61,17 @@ static duk_ret_t
 js_OpenRawFile(duk_context* ctx)
 {
 	int n_args = duk_get_top(ctx);
+	duk_require_string(ctx, 0);
+	if (n_args >= 2) duk_require_boolean(ctx, 1);
+
+	js_new_RawFile(ctx);
+	return 1;
+}
+
+static duk_ret_t
+js_new_RawFile(duk_context* ctx)
+{
+	int n_args = duk_get_top(ctx);
 	const char* filename = duk_require_string(ctx, 0);
 	bool writable = n_args >= 2 ? duk_require_boolean(ctx, 1) : false;
 
@@ -56,17 +82,9 @@ js_OpenRawFile(duk_context* ctx)
 	file = fopen(path, writable ? "w+b" : "rb");
 	free(path);
 	if (file == NULL)
-		duk_error_ni(ctx, -1, DUK_ERR_ERROR, "OpenRawFile(): Failed to open file '%s' for %s", filename, writable ? "writing" : "reading");
-	duk_push_object(ctx);
-	duk_push_pointer(ctx, file); duk_put_prop_string(ctx, -2, "\xFF" "file_ptr");
-	duk_push_c_function(ctx, js_RawFile_finalize, DUK_VARARGS); duk_set_finalizer(ctx, -2);
-	duk_push_c_function(ctx, js_RawFile_toString, DUK_VARARGS); duk_put_prop_string(ctx, -2, "toString");
-	duk_push_c_function(ctx, js_RawFile_getPosition, DUK_VARARGS); duk_put_prop_string(ctx, -2, "getPosition");
-	duk_push_c_function(ctx, js_RawFile_getSize, DUK_VARARGS); duk_put_prop_string(ctx, -2, "getSize");
-	duk_push_c_function(ctx, js_RawFile_setPosition, DUK_VARARGS); duk_put_prop_string(ctx, -2, "setPosition");
-	duk_push_c_function(ctx, js_RawFile_close, DUK_VARARGS); duk_put_prop_string(ctx, -2, "close");
-	duk_push_c_function(ctx, js_RawFile_read, DUK_VARARGS); duk_put_prop_string(ctx, -2, "read");
-	duk_push_c_function(ctx, js_RawFile_write, DUK_VARARGS); duk_put_prop_string(ctx, -2, "write");
+		duk_error_ni(ctx, -1, DUK_ERR_ERROR, "OpenRawFile(): Failed to open file '%s' for %s",
+			filename, writable ? "writing" : "reading");
+	duk_push_sphere_obj(ctx, "RawFile", file);
 	return 1;
 }
 
@@ -75,7 +93,7 @@ js_RawFile_finalize(duk_context* ctx)
 {
 	FILE* file;
 
-	duk_get_prop_string(ctx, 0, "\xFF" "file_ptr"); file = duk_get_pointer(ctx, -1); duk_pop(ctx);
+	file = duk_require_sphere_obj(ctx, 0, "RawFile");
 	if (file != NULL) fclose(file);
 	return 0;
 }
@@ -88,12 +106,12 @@ js_RawFile_toString(duk_context* ctx)
 }
 
 static duk_ret_t
-js_RawFile_getPosition(duk_context* ctx)
+js_RawFile_get_position(duk_context* ctx)
 {
 	FILE* file;
 
 	duk_push_this(ctx);
-	duk_get_prop_string(ctx, -1, "\xFF" "file_ptr"); file = duk_get_pointer(ctx, -1); duk_pop(ctx);
+	file = duk_require_sphere_obj(ctx, -1, "RawFile");
 	duk_pop(ctx);
 	if (file == NULL)
 		duk_error_ni(ctx, -1, DUK_ERR_ERROR, "RawFile:getPosition(): File has already been closed");
@@ -102,13 +120,30 @@ js_RawFile_getPosition(duk_context* ctx)
 }
 
 static duk_ret_t
-js_RawFile_getSize(duk_context* ctx)
+js_RawFile_set_position(duk_context* ctx)
+{
+	int new_pos = duk_require_int(ctx, 0);
+
+	FILE* file;
+
+	duk_push_this(ctx);
+	file = duk_require_sphere_obj(ctx, -1, "RawFile");
+	duk_pop(ctx);
+	if (file == NULL)
+		duk_error_ni(ctx, -1, DUK_ERR_ERROR, "RawFile:setPosition(): File has already been closed");
+	if (!fseek(file, new_pos, SEEK_SET))
+		duk_error_ni(ctx, -1, DUK_ERR_ERROR, "RawFile:setPosition(): Failed to set read/write position");
+	return 0;
+}
+
+static duk_ret_t
+js_RawFile_get_size(duk_context* ctx)
 {
 	FILE* file;
 	long  file_pos;
 
 	duk_push_this(ctx);
-	duk_get_prop_string(ctx, -1, "\xFF" "file_ptr"); file = duk_get_pointer(ctx, -1); duk_pop(ctx);
+	file = duk_require_sphere_obj(ctx, -1, "RawFile");
 	duk_pop(ctx);
 	if (file == NULL)
 		duk_error_ni(ctx, -1, DUK_ERR_ERROR, "RawFile:getPosition(): File has already been closed");
@@ -120,29 +155,12 @@ js_RawFile_getSize(duk_context* ctx)
 }
 
 static duk_ret_t
-js_RawFile_setPosition(duk_context* ctx)
-{
-	int new_pos = duk_require_int(ctx, 0);
-	
-	FILE* file;
-
-	duk_push_this(ctx);
-	duk_get_prop_string(ctx, -1, "\xFF" "file_ptr"); file = duk_get_pointer(ctx, -1); duk_pop(ctx);
-	duk_pop(ctx);
-	if (file == NULL)
-		duk_error_ni(ctx, -1, DUK_ERR_ERROR, "RawFile:setPosition(): File has already been closed");
-	if (!fseek(file, new_pos, SEEK_SET))
-		duk_error_ni(ctx, -1, DUK_ERR_ERROR, "RawFile:setPosition(): Failed to set read/write position");
-	return 0;
-}
-
-static duk_ret_t
 js_RawFile_close(duk_context* ctx)
 {
 	FILE* file;
 
 	duk_push_this(ctx);
-	duk_get_prop_string(ctx, -1, "\xFF" "file_ptr"); file = duk_get_pointer(ctx, -1); duk_pop(ctx);
+	file = duk_require_sphere_obj(ctx, -1, "RawFile");
 	duk_push_pointer(ctx, NULL); duk_put_prop_string(ctx, -2, "\xFF" "file_ptr");
 	duk_pop(ctx);
 	if (file == NULL)
@@ -156,12 +174,12 @@ js_RawFile_read(duk_context* ctx)
 {
 	size_t num_bytes = duk_require_uint(ctx, 0);
 
-	bytearray_t*  array;
-	FILE*         file;
-	void*         read_buffer;
+	bytearray_t* array;
+	FILE*        file;
+	void*        read_buffer;
 
 	duk_push_this(ctx);
-	duk_get_prop_string(ctx, -1, "\xFF" "file_ptr"); file = duk_get_pointer(ctx, -1); duk_pop(ctx);
+	file = duk_require_sphere_obj(ctx, -1, "RawFile");
 	duk_pop(ctx);
 	if (num_bytes <= 0 || num_bytes > INT_MAX)
 		duk_error_ni(ctx, -1, DUK_ERR_RANGE_ERROR, "RawFile:read(): Must read at least 1 byte and less than 2GB (requested: ~%i MB)", num_bytes / 1048576);
@@ -177,21 +195,47 @@ js_RawFile_read(duk_context* ctx)
 }
 
 static duk_ret_t
-js_RawFile_write(duk_context* ctx)
+js_RawFile_readString(duk_context* ctx)
 {
-	bytearray_t* array = duk_require_sphere_bytearray(ctx, 0);
-	
-	const void* data;
-	FILE*       file;
-	size_t      write_size;
+	size_t num_bytes = duk_require_uint(ctx, 0);
+
+	FILE* file;
+	void* read_buffer;
 
 	duk_push_this(ctx);
-	duk_get_prop_string(ctx, -1, "\xFF" "file_ptr"); file = duk_get_pointer(ctx, -1); duk_pop(ctx);
+	file = duk_require_sphere_obj(ctx, -1, "RawFile");
+	duk_pop(ctx);
+	if (num_bytes <= 0 || num_bytes > INT_MAX)
+		duk_error_ni(ctx, -1, DUK_ERR_RANGE_ERROR, "RawFile:read(): Read size out of range (%i)", num_bytes / 1048576);
+	if (file == NULL)
+		duk_error_ni(ctx, -1, DUK_ERR_ERROR, "RawFile:read(): File has already been closed");
+	if (!(read_buffer = malloc(num_bytes)))
+		duk_error_ni(ctx, -1, DUK_ERR_ERROR, "RawFile:read(): Failed to allocate buffer for file read");
+	num_bytes = fread(read_buffer, 1, num_bytes, file);
+	duk_push_lstring(ctx, read_buffer, num_bytes);
+	return 1;
+}
+
+static duk_ret_t
+js_RawFile_write(duk_context* ctx)
+{
+	bytearray_t* array;
+	const void*  data;
+	FILE*        file;
+	size_t       write_size;
+
+	duk_push_this(ctx);
+	file = duk_require_sphere_obj(ctx, -1, "RawFile");
 	duk_pop(ctx);
 	if (file == NULL)
 		duk_error_ni(ctx, -1, DUK_ERR_ERROR, "RawFile:write(): File has already been closed");
-	data = get_bytearray_buffer(array);
-	write_size = get_bytearray_size(array);
+	if (duk_is_string(ctx, 0))
+		data = duk_get_lstring(ctx, 0, &write_size);
+	else {
+		array = duk_require_sphere_obj(ctx, 0, "ByteArray");
+		data = get_bytearray_buffer(array);
+		write_size = get_bytearray_size(array);
+	}
 	if (fwrite(data, 1, write_size, file) != write_size)
 		duk_error_ni(ctx, -1, DUK_ERR_ERROR, "RawFile:write(): Write error. The file may be read-only.");
 	return 0;
