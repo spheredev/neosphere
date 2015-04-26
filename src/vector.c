51a336a4db775a3e2de2acd97439b79eca29aea9
Bruce Pascoe <fatcerberus@icloud.com>
2015-04-26 01:25:33 -0400

    Implement vector object
    Wow, this is levels of type-unsafe that even I'M uncomfortable with.
    And this is coming from an accomplished C programmer!  That's probably
    not a good sign...
    
    I'm going to commit this in an experimental branch for now until I'm
    confident it won't lead to a thousand annoying heisenbugs down the road.

---
+++ src/vector.c
@@ -0,0 +1,133 @@
+// WARNING: be careful when using this!
+// to cut down on the number of casts needed when using this generic vector implementation,
+// void pointers are thrown around with abandon. as a result, it is not type safe at all
+// and WILL blow up in your face if you're careless with it. you have been warned!
+
+#include "minisphere.h"
+
+#include "vector.h"
+
+static bool maybe_resize_vector(vector_t* vector, size_t min_items);
+
+struct vector
+{
+	size_t        pitch;
+	size_t        max_items;
+	size_t        num_items;
+	unsigned char *buffer;
+};
+
+vector_t*
+new_vector(size_t pitch)
+{
+	vector_t* vector;
+
+	if (!(vector = calloc(1, sizeof(vector_t))))
+		return NULL;
+	vector->pitch = pitch;
+	return vector;
+}
+
+void
+free_vector(vector_t* vector)
+{
+	free(vector->buffer);
+	free(vector);
+}
+
+void
+get_vector_item(const vector_t* vector, size_t index, void* out_object)
+{
+	unsigned char* p;
+
+	p = vector->buffer + index * vector->pitch;
+	memcpy(out_object, p, vector->pitch);
+}
+
+void
+set_vector_item(vector_t* vector, size_t index, const void* in_object)
+{
+	unsigned char* p;
+
+	p = vector->buffer + index * vector->pitch;
+	memcpy(p, in_object, vector->pitch);
+}
+
+size_t
+get_vector_size(const vector_t* vector)
+{
+	return vector->num_items;
+}
+
+void
+clear_vector(vector_t* vector)
+{
+	vector->num_items = 0;
+	maybe_resize_vector(vector, 0);
+}
+
+iter_t*
+iterate_vector(const vector_t* vector)
+{
+	return (iter_t*)vector->buffer;
+}
+
+bool
+next_vector_item(const vector_t* vector, iter_t** inout_iter, void* out_object)
+{
+	unsigned char* p_current;
+	unsigned char* p_tail;
+
+	p_tail = vector->buffer + vector->num_items * vector->pitch;
+	p_current = (unsigned char*)*inout_iter;
+	if (p_current == NULL || p_current >= p_tail) {
+		*inout_iter = NULL;
+		return false;
+	}
+	memcpy(out_object, p_current, vector->pitch);
+	*inout_iter = (iter_t*)(p_current + vector->pitch);
+	return true;
+}
+
+bool
+push_back_vector(vector_t* vector, const void* in_object)
+{
+	if (!maybe_resize_vector(vector, vector->num_items + 1))
+		return false;
+	memcpy(vector->buffer + vector->num_items * vector->pitch, in_object, vector->pitch);
+	++vector->num_items;
+	return true;
+}
+
+void
+remove_vector_item(vector_t* vector, size_t index)
+{
+	size_t         move_size;
+	unsigned char* p;
+
+	--vector->num_items;
+	move_size = (vector->num_items - index) * vector->pitch;
+	p = vector->buffer + index * vector->pitch;
+	memmove(p, p + vector->pitch, move_size);
+	maybe_resize_vector(vector, vector->num_items);
+}
+
+static bool
+maybe_resize_vector(vector_t* vector, size_t min_items)
+{
+	unsigned char* new_buffer;
+	size_t         new_max;
+	
+	new_max = vector->max_items;
+	if (min_items > vector->max_items)  // is the buffer too small?
+		new_max = min_items * 2;
+	else if (min_items < vector->max_items / 4)  // if item count drops below 1/4 of peak size, shrink the buffer
+		new_max = min_items * 2;
+	if (new_max != vector->max_items) {
+		if (!(new_buffer = realloc(vector->buffer, new_max * vector->pitch)))
+			return false;
+		vector->buffer = new_buffer;
+		vector->max_items = new_max;
+	}
+	return true;
+}
---
+++ src/vector.h
@@ -0,0 +1,18 @@
+#ifndef MINISPHERE__VECTOR_H__INCLUDED
+#define MINISPHERE__VECTOR_H__INCLUDED
+
+typedef struct vector vector_t;
+typedef struct iter   iter_t;
+
+vector_t* new_vector         (size_t pitch);
+void      free_vector        (vector_t* vector);
+void      get_vector_item    (const vector_t* vector, size_t index, void* out_object);
+void      set_vector_item    (vector_t* vector, size_t index, const void* in_object);
+size_t    get_vector_size    (const vector_t* vector);
+void      clear_vector       (vector_t* vector);
+iter_t*   iterate_vector     (const vector_t* vector);
+bool      next_vector_item   (const vector_t* vector, iter_t** inout_iter, void* out_object);
+bool      push_back_vector   (vector_t* vector, const void* in_object);
+void      remove_vector_item (vector_t* vector, size_t index);
+
+#endif // MINISPHERE__VECTOR_H__INCLUDED
