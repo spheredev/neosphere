993d508f963d12df537c12d5e1d69852c4f1ba1a
Bruce Pascoe <fatcerberus@icloud.com>
2015-05-05 23:09:28 -0400

    minisphere 1.1b4
    I'm hoping this will be the last beta of 1.1.  Just have to finish up
    the API documentation, flesh out the minisphere Runtime a bit more, and
    implement the config tool.  That last thing is going to be a pain...

--- src/minisphere.h
+++ src/minisphere.h
@@ -40,7 +40,7 @@
 #define noreturn void
 #endif
 
-static const char* const ENGINE_NAME = "minisphere 1.1b3";
+static const char* const ENGINE_NAME = "minisphere 1.1b4";
 
 extern ALLEGRO_DISPLAY*     g_display;
 extern ALLEGRO_EVENT_QUEUE* g_events;
