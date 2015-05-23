cc5a3ad9eb5fb980cf177ac50caffd9976e3f821
Bruce Pascoe <fatcerberus@icloud.com>
2015-05-23 11:20:45 -0400

    minisphere 1.0.11
    After all, if I'm going to call the 1.0 series "stable", I'm going to
    have to maintain it...

--- minisphere/minisphere.h
+++ minisphere/minisphere.h
@@ -36,14 +36,9 @@
 #define noreturn void
 #endif
 
-static const char* const ENGINE_NAME        = "minisphere 1.0.10";
+static const char* const ENGINE_NAME        = "minisphere 1.0.11";
+static const char* const SPHERE_USERAGENT   = "v1.5 (compatible; minisphere 1.0.11)";
 static double      const SPHERE_API_VERSION = 1.5;
-
-#if defined(__x86_64__) || defined(_M_X64)
-static const char* const SPHERE_USERAGENT   = "v1.5 (compatible; minisphere 1.0.10 x64)";
-#else
-static const char* const SPHERE_USERAGENT = "v1.5 (compatible; minisphere 1.0.10)";
-#endif
 
 
 static const char* const EXTENSIONS[] = {
