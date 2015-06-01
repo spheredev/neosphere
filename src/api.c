9a4693ede11998f198d7aab9ead634cc064bb86a
Bruce Pascoe <fatcerberus@icloud.com>
2015-06-01 01:53:17 -0400

    Add CommonJS documentation
    Also added an extension, `sphere-commonjs`, so games can query whether
    the engine supports modules.

--- src/api.c
+++ src/api.c
@@ -13,6 +13,7 @@ static const char* const SPHERE_EXTENSIONS[] =
 	"sphere-obj-props",
 	"sphere-map-engine",
 	"sphere-galileo",
+	"sphere-commonjs",
 	"minisphere-async-api",
 	"minisphere-new-sockets",
 	"minisphere-rng-object",
