533fc8812af62bae0c4f10d9f33bdb40236ac5a0
Bruce Pascoe <fatcerberus@icloud.com>
2015-09-14 13:50:45 -0400

    Implement file installation in Cell
    This is actually a bit sloppy, but it should be sufficient for testing
    for now.

--- cell/main.c
+++ cell/main.c
@@ -9,10 +9,14 @@
 #include <time.h>
 
 #include "duktape.h"
+#include "tinydir.h"
 
 #define CELL_VERSION "2.0.0"
 
-static duk_ret_t js_game (duk_context* ctx);
+static duk_ret_t js_game    (duk_context* ctx);
+static duk_ret_t js_install (duk_context* ctx);
+
+static bool wildcmp (const char* filename, const char* pattern);
 
 static duk_context* s_duk = NULL;
 static bool         s_want_spk = false;
@@ -42,15 +46,17 @@ main(int argc, char* argv[])
 
 	srand((unsigned int)time(NULL));
 	num_messages = sizeof MESSAGES / sizeof(const char*);
-
+	
 	printf("Cell %s  (c) 2015 Fat Cerberus\n", CELL_VERSION);
 	printf("%s\n", MESSAGES[rand() % num_messages]);
 	
 	// initialize JavaScript environment
 	s_duk = duk_create_heap_default();
 	duk_push_c_function(s_duk, js_game, DUK_VARARGS);
+	duk_push_c_function(s_duk, js_install, DUK_VARARGS);
+	duk_put_global_string(s_duk, "install");
 	duk_put_global_string(s_duk, "game");
-	
+
 	// evaluate the build script
 	if (duk_pcompile_file(s_duk, 0x0, "cell.js") != DUK_EXEC_SUCCESS
 		|| duk_pcall(s_duk, 0) != DUK_EXEC_SUCCESS)
@@ -76,7 +82,7 @@ main(int argc, char* argv[])
 		printf("Success!\n");
 	}
 	else {
-		fprintf(stderr, "ERROR: Nothing to do for target '%s'\n", target_name);
+		fprintf(stderr, "ERROR: No target named '%s'\n", target_name);
 		retval = EXIT_FAILURE;
 		goto shutdown;
 	}
@@ -84,6 +90,103 @@ main(int argc, char* argv[])
 shutdown:
 	duk_destroy_heap(s_duk);
 	return retval;
+}
+
+int
+install(const char* pattern, const char* src_path, const char* dest_path, bool recursive)
+{
+	tinydir_dir  dir;
+	tinydir_file file;
+	int          num_files = 0;
+	char         path[1024];
+
+	tinydir_open(&dir, src_path);
+	while (dir.has_next) {
+		tinydir_readfile(&dir, &file);
+		if (!file.is_dir && wildcmp(file.name, pattern)) {
+			sprintf(path, "./dist/%s/", dest_path);
+			CreateDirectoryA(path, NULL);
+			strcat(path, file.name);
+			if (CopyFileA(file.path, path, TRUE))
+				++num_files;
+		}
+		else if (file.is_dir && recursive
+			&& strcmp(file.name, ".") != 0 && strcmp(file.name, "..") != 0
+			&& strcmp(file.path, "./dist") != 0)
+		{
+			sprintf(path, "%s/%s", dest_path, file.name);
+			install(pattern, file.path, path, recursive);
+		}
+		tinydir_next(&dir);
+	}
+	tinydir_close(&dir);
+	if (num_files > 0)
+		printf("Installed %d file(s) to ./dist/%s\n", num_files, dest_path);
+	return num_files;
+}
+
+static bool
+wildcmp(const char *string, const char *pattern)
+{
+	const char* cp = NULL;
+	const char* mp = NULL;
+
+	while (*string != '\0' && *pattern != '*') {
+		if (*pattern != *string && *pattern != '?')
+			return false;
+		++pattern;
+		++string;
+	}
+	while (*string != '\0') {
+		if (*pattern == '*') {
+			if (*++pattern == '\0') return true;
+			mp = pattern;
+			cp = string + 1;
+		}
+		else if (*pattern == *string || *pattern == '?') {
+			pattern++;
+			string++;
+		}
+		else {
+			pattern = mp;
+			string = cp++;
+		}
+	}
+	while (*pattern == '*')
+		pattern++;
+	return *pattern == '\0';
+}
+
+static duk_ret_t
+js_install(duk_context* ctx)
+{
+	// install(pattern, path, isRecursive);
+	// Copies file(s) from source into target
+	// Arguments:
+	//     pattern:   A filename pattern, e.g. "*.js". Files matching the pattern
+	//                will be copied to the destination directory.
+	//     path:      Path to the destination, relative to the output directory.
+	//     recursive: Optional. true to recursively copy files in subdirectories.
+	//                (default: true)
+	
+	const char*  dest_path;
+	bool         is_recursive;
+	char*        last_slash;
+	int          n_args;
+	char*        pattern;
+	const char*  src_path;
+
+	n_args = duk_get_top(ctx);
+	pattern = strdup(duk_require_string(ctx, 0));
+	dest_path = n_args >= 2 ? duk_require_string(ctx, 1) : ".";
+	is_recursive = n_args >= 3 ? duk_is_valid_index(ctx, 2) : true;
+	
+	if (last_slash = strrchr(pattern, '/'))
+		*last_slash = '\0';
+	src_path = last_slash != NULL ? pattern : ".";
+	pattern = last_slash != NULL ? last_slash + 1 : pattern;
+	install(pattern, src_path, dest_path, is_recursive);
+	return 0;
 }
 
 static duk_ret_t
@@ -111,12 +214,14 @@ js_game(duk_context* ctx)
 	res_y = atoi(strtok(NULL, "x"));
 
 	// write game.sgm
-	if (!(file = fopen("game.sgm", "wb")))
+	if (!(file = fopen("dist/game.sgm", "wb")))
 		duk_error(ctx, DUK_ERR_ERROR, "Failed to write game.sgm");
-	fprintf(file, "name=%s", name);
+	fprintf(file, "name=%s\n", name);
 	fprintf(file, "author=%s\n", author);
 	fprintf(file, "description=%s\n", description);
-	fprintf(file, "resolution=%s\n", author);
+	fprintf(file, "screen_width=%d\n", res_x);
+	fprintf(file, "screen_height=%d\n", res_y);
+	fprintf(file, "script=%s\n", script);
 	fclose(file);
 	printf("Wrote game.sgm '%s'\n", name);
 	return 0;
