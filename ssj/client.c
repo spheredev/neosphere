fb84ff30e7d00c85d62d2ae77f30af72c2866a08
Bruce Pascoe <fatcerberus@icloud.com>
2016-01-10 02:19:47 -0500

    SSJ: Add some more dvalue parsing
    I'm thinking the best approach will ultimately be to split the Duktape
    client code into two layers: The base dvalue protocol, which sends and
    receives dvalues, with the actual Duktape command processing as a
    separate component.
    
    The rationale for this is that the C# client conflates these two layers,
    and code complexity suffers for it.  DuktapeClient.cs isn't exactly fun
    to maintain.

--- ssj/client.c
+++ ssj/client.c
@@ -5,31 +5,43 @@
 
 enum dvalue_tag
 {
-	DVALUE_EOM,
-	DVALUE_REQ,
-	DVALUE_REP,
-	DVALUE_ERR,
-	DVALUE_NFY,
-	DVALUE_UNUSED,
-	DVALUE_UNDEF,
-	DVALUE_INT,
-	DVALUE_STRING,
-	DVALUE_FLOAT,
-	DVALUE_NULL,
-	DVALUE_OBJ,
-	DVALUE_HEAPPTR,
-	DVALUE_PTR,
-	DVALUE_LIGHTFUNC,
+	DVALUE_TAG_EOM = 0x00,
+	DVALUE_TAG_REQ = 0x01,
+	DVALUE_TAG_REP = 0x02,
+	DVALUE_TAG_ERR = 0x03,
+	DVALUE_TAG_NFY = 0x04,
+	DVALUE_TAG_INT32 = 0x10,
+	DVALUE_TAG_STR32 = 0x11,
+	DVALUE_TAG_STR16 = 0x12,
+	DVALUE_TAG_BUF32 = 0x13,
+	DVALUE_TAG_BUF16 = 0x14,
+	DVALUE_TAG_UNUSED = 0x15,
+	DVALUE_TAG_UNDEF = 0x16,
+	DVALUE_TAG_NULL = 0x17,
+	DVALUE_TAG_TRUE = 0x18,
+	DVALUE_TAG_FALSE = 0x19,
+	DVALUE_TAG_FLOAT = 0x1A,
+	DVALUE_TAG_OBJ = 0x1B,
+	DVALUE_TAG_PTR = 0x1C,
+	DVALUE_TAG_LIGHTFUNC = 0x1D,
+	DVALUE_TAG_HEAPPTR = 0x1E,
+
+	// Special cases:
+	// 0x60...0x7F - string, len = (ib - 0x60)
+	// 0x80...0xBF - int, value = (ib - 0x80)
+	// 0xC0...0xFF - int, value = ((b[0] - 0xC0) << 8) + b[1]
 };
 
 struct session
 {
 	uint8_t*     recv_buf;
 	size_t       recv_buf_size;
-	size_t       num_bytes;
+	size_t       recv_size;
 	dyad_Stream* socket;
 	path_t*      source_root;
 	vector_t*    dvalues;
+	unsigned int req_id;
+	unsigned int rep_id;
 };
 
 struct dvalue
@@ -53,32 +65,49 @@ on_socket_recv(dyad_Event* e)
 	char*      p_next_write;
 	
 	session = e->udata;
-	while (session->num_bytes + e->size > session->recv_buf_size) {
+	while (session->recv_size + e->size > session->recv_buf_size) {
 		session->recv_buf_size *= 2;
 		need_resize = true;
 	}
 	if (need_resize)
 		session->recv_buf = realloc(session->recv_buf, session->recv_buf_size);
-	p_next_write = session->recv_buf + session->num_bytes;
-	session->num_bytes += e->size;
+	p_next_write = session->recv_buf + session->recv_size;
+	session->recv_size += e->size;
 	memcpy(p_next_write, e->data, e->size);
 }
 
 static size_t
 receive_all(session_t* session, void* buffer, size_t num_bytes)
 {
-	num_bytes = num_bytes >= session->num_bytes ? num_bytes : session->num_bytes;
+	while (session->recv_size < num_bytes)
+		dyad_update();
 	memcpy(buffer, session->recv_buf, num_bytes);
 	memmove(session->recv_buf, session->recv_buf + num_bytes,
-		session->num_bytes -= num_bytes);
+		session->recv_size -= num_bytes);
 	return num_bytes;
+}
+
+static void
+send_dvalue_ib(session_t* session, enum dvalue_tag tag)
+{
+	uint8_t ib;
+
+	ib = tag == DVALUE_TAG_EOM ? 0x00
+		: tag == DVALUE_TAG_REQ ? 0x01
+		: tag == DVALUE_TAG_REP ? 0x02
+		: tag == DVALUE_TAG_ERR ? 0x03
+		: tag == DVALUE_TAG_NFY ? 0x04
+		: tag == DVALUE_TAG_INT32 ? 0x10
+		: tag == DVALUE_TAG_STR32 ? 0x11
+		: tag == DVALUE_TAG_FLOAT ? 0x1A
+		: 0x15;
+	dyad_write(session->socket, &ib, 1);
 }
 
 void
 initialize_client(void)
 {
 	dyad_init();
-	dyad_setUpdateTimeout(0.0);
 }
 
 void
@@ -102,7 +131,7 @@ session_new(const char* hostname, int port)
 	session->socket = socket;
 	session->recv_buf_size = 65536;
 	session->recv_buf = malloc(session->recv_buf_size);
-	session->num_bytes = 0;
+	session->recv_size = 0;
 	session->dvalues = vector_new(sizeof(struct dvalue));
 	return session;
 
@@ -121,44 +150,75 @@ session_free(session_t* session)
 }
 
 void
-session_tick(session_t* session)
+session_update(session_t* session)
 {
+	uint8_t       data[8];
 	struct dvalue dvalue;
 	uint8_t       ib;
+	uint8_t*      p_number;
 	
 	dyad_update();
-	if (session->num_bytes > 0) {
-		memset(&dvalue, 0, sizeof(struct dvalue));
-		receive_all(session, &ib, 1);
-		switch (ib) {
-		case 0x00: dvalue.tag = DVALUE_EOM; break;
-		case 0x01: dvalue.tag = DVALUE_REQ; break;
-		case 0x02: dvalue.tag = DVALUE_REP; break;
-		case 0x03: dvalue.tag = DVALUE_ERR; break;
-		case 0x04: dvalue.tag = DVALUE_NFY; break;
-		default:
-			dvalue.tag = DVALUE_UNUSED;
-			break;
-		}
-		vector_push(session->dvalues, &dvalue);
+	if (session->recv_size > 0) {
+		do {
+			memset(&dvalue, 0, sizeof(struct dvalue));
+			receive_all(session, &ib, 1);
+			switch (dvalue.tag) {
+			case DVALUE_TAG_EOM:
+			case DVALUE_TAG_REQ:
+			case DVALUE_TAG_REP:
+			case DVALUE_TAG_ERR:
+			case DVALUE_TAG_NFY:
+				dvalue.tag = (enum dvalue_tag)ib;
+				break;
+			case DVALUE_TAG_INT32:
+				dvalue.tag = DVALUE_TAG_INT32;
+				receive_all(session, data, 4);
+				dvalue.number = (double)((data[0] << 24) + (data[1] << 16)
+					+ (data[2] << 8) + data[3]);
+				break;
+			case DVALUE_TAG_STR32:
+				dvalue.tag = DVALUE_TAG_STR32;
+				receive_all(session, data, 4);
+				dvalue.buffer.size = (data[0] << 24) + (data[1] << 16) + (data[2] << 8) + data[3];
+				dvalue.buffer.data = calloc(1, dvalue.buffer.size + 1);
+				receive_all(session, dvalue.buffer.data, dvalue.buffer.size);
+				break;
+			case DVALUE_TAG_STR16:
+				dvalue.tag = DVALUE_TAG_STR32;
+				receive_all(session, data, 2);
+				dvalue.buffer.size = (data[0] << 8) + data[1];
+				dvalue.buffer.data = calloc(1, dvalue.buffer.size + 1);
+				receive_all(session, dvalue.buffer.data, dvalue.buffer.size);
+				break;
+			case DVALUE_TAG_UNUSED:
+			case DVALUE_TAG_UNDEF:
+			case DVALUE_TAG_NULL:
+			case DVALUE_TAG_TRUE:
+			case DVALUE_TAG_FALSE:
+				dvalue.tag = (enum dvalue_tag)ib;
+				break;
+			case DVALUE_TAG_FLOAT:
+				receive_all(session, data, 8);
+				p_number = (uint8_t*)&dvalue.number;
+				p_number[0] = data[7];
+				p_number[1] = data[6];
+				p_number[2] = data[5];
+				p_number[3] = data[4];
+				p_number[4] = data[3];
+				p_number[5] = data[2];
+				p_number[6] = data[1];
+				p_number[7] = data[0];
+				break;
+			}
+			vector_push(session->dvalues, &dvalue);
+		} while (dvalue.tag != DVALUE_TAG_EOM);
 	}
 }
 
-void
-send_dvalue_ib(session_t* session, enum dvalue_tag tag)
+dvalue_t*
+receive_dvalue(session_t* session)
 {
-	uint8_t ib;
 
-	ib = tag == DVALUE_EOM ? 0x00
-		: tag == DVALUE_REQ ? 0x01
-		: tag == DVALUE_REP ? 0x02
-		: tag == DVALUE_ERR ? 0x03
-		: tag == DVALUE_NFY ? 0x04
-		: tag == DVALUE_INT ? 0x10
-		: tag == DVALUE_STRING ? 0x11
-		: tag == DVALUE_FLOAT ? 0x1A
-		: 0x15;
-	dyad_write(session->socket, &ib, 1);
 }
 
 void
@@ -174,7 +234,7 @@ send_dvalue_string(session_t* session, const char* string)
 	len_data[2] = (uint8_t)(length >> 8 & 0xFF);
 	len_data[3] = (uint8_t)(length & 0xFF);
 	
-	send_dvalue_ib(session, DVALUE_STRING);
+	send_dvalue_ib(session, DVALUE_TAG_STR32);
 	dyad_write(session->socket, len_data, 4);
 	dyad_write(session->socket, string, (int)length);
 }
@@ -190,7 +250,7 @@ send_dvalue_int(session_t* session, int32_t value)
 	data[2] = (uint8_t)(value >> 8 & 0xFF);
 	data[3] = (uint8_t)(value & 0xFF);
 	
-	send_dvalue_ib(session, DVALUE_INT);
+	send_dvalue_ib(session, DVALUE_TAG_INT32);
 	dyad_write(session->socket, data, 4);
 }
 
@@ -209,6 +269,6 @@ send_dvalue_float(session_t* session, double value)
 	data[6] = ((uint8_t*)&value)[1];
 	data[7] = ((uint8_t*)&value)[0];
 
-	send_dvalue_ib(session, DVALUE_FLOAT);
+	send_dvalue_ib(session, DVALUE_TAG_FLOAT);
 	dyad_write(session->socket, data, 8);
 }
--- ssj/client.h
+++ ssj/client.h
@@ -2,8 +2,23 @@
 #define SSJ__CLIENT_H__INCLUDED
 
 typedef struct session session_t;
+typedef struct dvalue  dvalue_t;
 
-void initialize_client (void);
-void shutdown_client   (void);
+typedef
+enum dvalue_type
+{
+	DVALUE_BOOLEAN,
+	DVALUE_FLOAT,
+	DVALUE_INT,
+	DVALUE_STRING,
+} dvalue_type_t;
+
+void          initialize_client (void);
+void          shutdown_client   (void);
+dvalue_t*     dvalue_new_float  (double value);
+dvalue_t*     dvalue_new_int    (int32_t value);
+dvalue_t*     dvalue_new_string (const char* value);
+void          dvalue_free       (dvalue_t* dvalue);
+dvalue_type_t dvalue_get_type   (dvalue_t* dvalue);
 
 #endif // SSJ__CLIENT_H__INCLUDED
