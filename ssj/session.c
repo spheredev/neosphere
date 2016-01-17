53cc1898a827d22870ea703f1769ffb41632b826
Bruce Pascoe <fatcerberus@icloud.com>
2016-01-17 11:42:29 -0500

    Implement Eval command
    Also fixed a regression where SSJ would continue to wait for messages
    after the Detaching notify is received, locking up the program.

--- ssj/session.c
+++ ssj/session.c
@@ -9,7 +9,7 @@ struct session
 {
 	char      cl_buffer[CL_BUFFER_SIZE];
 	remote_t* remote;
-	bool      is_paused;
+	bool      want_cl;
 };
 
 enum req_command
@@ -46,7 +46,7 @@ enum nfy_command
 
 static void       do_command_line (session_t* sess);
 static message_t* handle_req      (session_t* sess, const message_t* msg);
-static void       process_message (session_t* sess, const message_t* msg);
+static bool       process_message (session_t* sess, const message_t* msg);
 
 session_t*
 new_session(const char* hostname, int port)
@@ -66,15 +66,18 @@ on_error:
 void
 run_session(session_t* sess)
 {
+	bool       is_active = true;
 	message_t* msg;
 	
-	while (true) {
-		if (!(msg = msg_receive(sess->remote)))
-			goto on_error;
-		process_message(sess, msg);
-		msg_free(msg);
-		if (sess->is_paused)
+	while (is_active) {
+		if (sess->want_cl)
 			do_command_line(sess);
+		else {
+			if (!(msg = msg_receive(sess->remote)))
+				goto on_error;
+			is_active = process_message(sess, msg);
+			msg_free(msg);
+		}
 	}
 	return;
 
@@ -87,17 +90,20 @@ on_error:
 static void
 do_command_line(session_t* sess)
 {
-	char*      argument;
-	char*      command;
-	int        ch = '\0';
-	size_t     ch_idx = 0;
-	char*      parsee;
-	message_t* req;
+	char*       argument;
+	char*       command;
+	int         ch = '\0';
+	size_t      ch_idx = 0;
+	lstring_t*  eval_code;
+	const char* eval_result;
+	char*       parsee;
+	message_t*  reply;
+	message_t*  req;
 
 	// get a command from the user
 	sess->cl_buffer[0] = '\0';
 	while (sess->cl_buffer[0] == '\0') {
-		printf("SSJ: ");
+		printf("ssj: ");
 		ch = getchar();
 		while (ch != '\n') {
 			if (ch_idx >= CL_BUFFER_SIZE - 1) {
@@ -115,9 +121,24 @@ do_command_line(session_t* sess)
 	parsee = strdup(sess->cl_buffer);
 	command = strtok_r(parsee, " ", &argument);
 	if (strcmp(command, "x") == 0) {
+		printf("Exit requested, detaching debugger.\n");
 		req = msg_new(MSG_CLASS_REQ);
 		msg_add_int(req, REQ_DETACH);
 		msg_free(handle_req(sess, req));
+		msg_free(req);
+		sess->want_cl = false;
+	}
+	else if (strcmp(command, "e") == 0) {
+		eval_code = lstr_newf(
+			"(function() { try { return Duktape.enc('jx', eval(\"%s\"), null, 3); } catch (e) { return e.toString(); } }).call(this);",
+			argument);
+		req = msg_new(MSG_CLASS_REQ);
+		msg_add_int(req, REQ_EVAL);
+		msg_add_string(req, lstr_cstr(eval_code));
+		reply = handle_req(sess, req);
+		msg_get_string(reply, 1, &eval_result);
+		printf("%s\n\n", eval_result);
+		msg_free(reply);
 		msg_free(req);
 	}
 	else if (strcmp(command, "r") == 0) {
@@ -125,6 +146,7 @@ do_command_line(session_t* sess)
 		msg_add_int(req, REQ_RESUME);
 		msg_free(handle_req(sess, req));
 		msg_free(req);
+		sess->want_cl = false;
 	}
 	free(parsee);
 }
@@ -145,7 +167,7 @@ handle_req(session_t* sess, const message_t* msg)
 	return response;
 }
 
-static void
+static bool
 process_message(session_t* sess, const message_t* msg)
 {
 	const char* filename;
@@ -165,7 +187,7 @@ process_message(session_t* sess, const message_t* msg)
 			msg_get_string(msg, 2, &filename);
 			msg_get_string(msg, 3, &func_name);
 			msg_get_int(msg, 4, &line_no);
-			if (sess->is_paused = state != 0) {
+			if (sess->want_cl = state != 0) {
 				printf("Paused in function '%s'\n", func_name);
 				printf("  script: '%s':%i\n\n", filename, line_no);
 			}
@@ -182,8 +204,9 @@ process_message(session_t* sess, const message_t* msg)
 			break;
 		case NFY_DETACHING:
 			printf("Target has been detached.\n");
-			break;
+			return false;
 		}
 		break;
 	}
+	return true;
 }
--- ssj/ssj.h
+++ ssj/ssj.h
@@ -14,6 +14,7 @@
 #include <string.h>
 #include <time.h>
 
+#include "lstring.h"
 #include "path.h"
 #include "vector.h"
 
