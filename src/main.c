6cb4fd30df0d9e638dd1f9a069e83b809f567c8a
Bruce Pascoe <fatcerberus@icloud.com>
2016-01-19 01:08:10 -0500

    Add a workaround for Allegro's title text bug
    This is fixed in the latest Allegro builds, but that doesn't help for
    Linux where using self-built Allegro binaries isn't really feasible,
    particularly if I ever want to make official packages.

--- src/main.c
+++ src/main.c
@@ -93,6 +93,7 @@ main(int argc, char* argv[])
 	image_t*             icon;
 	int                  line_num;
 	const path_t*        script_path;
+	lstring_t*           title_text;
 	ALLEGRO_TRANSFORM    trans;
 	int                  verbosity;
 	bool                 want_debug;
@@ -176,15 +177,19 @@ main(int argc, char* argv[])
 	if (!verify_requirements(g_fs))
 		exit_game(false);
 
-	get_sgm_resolution(g_fs, &g_res_x, &g_res_y);
+	// Allegro 5.1.12 has a bug where the the last character of the title bar text is cut
+	// off, so we add a space to compensate.
+	title_text = lstr_newf("%s ", get_sgm_name(g_fs));
+	al_set_new_window_title(lstr_cstr(title_text));
+	lstr_free(title_text);
 
 	// try to create a display. if we can't get a programmable pipeline, try again but
 	// only request bare OpenGL. keep in mind that if this happens, shader support will be
 	// disabled.
 	console_log(1, "Creating Allegro rendering context");
+	get_sgm_resolution(g_fs, &g_res_x, &g_res_y);
 	g_scale_x = g_scale_y = (g_res_x <= 400 && g_res_y <= 300) ? 2.0 : 1.0;
 	console_log(1, "  Resolution: %dx%d @ %.1fx", g_res_x, g_res_y, (double)g_scale_x);
-		al_set_new_window_title(get_sgm_name(g_fs));
 	al_set_new_display_flags(ALLEGRO_OPENGL | ALLEGRO_PROGRAMMABLE_PIPELINE);
 	if (!(g_display = al_create_display(g_res_x * g_scale_x, g_res_y * g_scale_y))) {
 		al_set_new_display_flags(ALLEGRO_OPENGL);
