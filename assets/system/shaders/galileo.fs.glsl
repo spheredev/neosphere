3cec63e12d636389c240327849b186d4d9848e0f
Bruce Pascoe <fatcerberus@icloud.com>
2016-05-02 14:04:38 -0400

    Process color matrix in fragment shader
    Doing it in the vertex shader doesn't get me the proper behavior -
    vertex colors only serve to _tint_ the polygons; fragment colors have to
    be manipulated directly to get the expected effect.

--- assets/system/shaders/galileo.fs.glsl
+++ assets/system/shaders/galileo.fs.glsl
@@ -8,16 +8,28 @@
 precision mediump float;
 #endif
 
+// when adapting this shader, the uniforms below need to be kept.  if you
+// remove them, you won't be able to make a useful shader for minisphere.
 uniform sampler2D al_tex;
 uniform bool al_use_tex;
+uniform mat4 colormatrix;
 
 varying vec4 varying_color;
 varying vec2 varying_texcoord;
 
 void main()
 {
-	if (al_use_tex)
-		gl_FragColor = varying_color * texture2D(al_tex, varying_texcoord);
-	else
-		gl_FragColor = varying_color;
+	vec4 color = al_use_tex
+        ? varying_color * texture2D(al_tex, varying_texcoord)
+        : varying_color;
+
+	// set alpha = 1.0 before applying the color matrix so that alpha doesn't
+	// skew translations.  this is about the best we can do since OpenGL
+	// doesn't support 5x5 matrices.
+    float old_alpha = color.a;
+    color.a = 1.0;
+    color = colormatrix * color;
+    color.a = old_alpha;
+
+    gl_FragColor = color;
 }
--- assets/system/shaders/galileo.vs.glsl
+++ assets/system/shaders/galileo.vs.glsl
@@ -11,21 +11,12 @@ attribute vec4 al_pos;
 attribute vec4 al_color;
 attribute vec2 al_texcoord;
 uniform mat4 al_projview_matrix;
-uniform mat4 colormatrix;
 
 varying vec4 varying_color;
 varying vec2 varying_texcoord;
 
 void main()
 {
-	// set alpha = 1.0 before applying the color matrix so that alpha
-	// doesn't influence translations.  this is about the best we can do since
-	// OpenGL doesn't support 5x5 matrices.
-	vec4 color = vec4(al_color.rgb, 1.0);
-	float alpha = al_color.a;
-	color = colormatrix * color; color.a = alpha;
-
-	// calculate vertex and set up varyings for the fragment shader
 	gl_Position = al_projview_matrix * al_pos;
 	varying_color = color;
 	varying_texcoord = al_texcoord;
