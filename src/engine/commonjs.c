616e2cd5f65efce0ad944467cfc9a976a61de060
Bruce Pascoe <fatcerberus@icloud.com>
2016-05-19 23:59:19 -0400

    Implement support for __filename and __dirname
    I'm trying to mimic Node.js as much as possible.  Why these exist when
    `module.filename` is a thing is completely beyond me, though.
    
    Also added .coffee and .ts extensions to the list of filename patterns
    searched when looking for modules.

--- src/engine/commonjs.c
+++ src/engine/commonjs.c
@@ -6,8 +6,10 @@
 
 static duk_ret_t js_require (duk_context* ctx);
 
+static path_t* find_module (const char* id, const char* origin, const char* sys_origin);
+
 duk_int_t
-duk_peval_module(const char* filename)
+cjs_eval_module(const char* filename)
 {
 	// HERE BE DRAGONS!
 	// this function is horrendous.  Duktape's stack-based API is powerful, but gets
@@ -18,13 +20,16 @@ duk_peval_module(const char* filename)
 	// notes:
 	//     - the final value of `module.exports` is left on top of the Duktape value stack.
 	//     - `module.id` is set to the given filename.  in order to guarantee proper cache
-	//       behavior, the filename should be canonicalized first.
+	//       behavior, the filename should be in canonical form.
 	//     - if the module code throws, the exception will propogate out of this call.
 
 	lstring_t* code_string;
+	path_t*    dir_path;
 	size_t     source_size;
 	char*      source;
 
+	dir_path = path_strip(path_new(filename));
+	
 	// is the requested module already in the cache?
 	duk_push_global_stash(g_duk);
 	duk_get_prop_string(g_duk, -1, "moduleCache");
@@ -46,9 +51,9 @@ duk_peval_module(const char* filename)
 		lstr_free(code_string);
 		return DUK_EXEC_ERROR;
 	}
-	duk_push_string(g_duk, "(function(exports,require,module){");
+	duk_push_string(g_duk, "(function module(exports, require, module, __filename, __dirname) { ");
 	duk_push_lstring_t(g_duk, code_string);
-	duk_push_string(g_duk, "})");
+	duk_push_string(g_duk, " })");
 	duk_concat(g_duk, 3);
 	duk_push_string(g_duk, filename);
 	duk_compile(g_duk, DUK_COMPILE_EVAL);
@@ -90,41 +95,47 @@ duk_peval_module(const char* filename)
 	duk_push_string(g_duk, filename);
 	duk_def_prop(g_duk, -3, DUK_DEFPROP_HAVE_VALUE);  // require.id
 	duk_dup(g_duk, -4);  // module
+	duk_push_string(g_duk, filename);  // __filename
+	duk_push_string(g_duk, path_cstr(dir_path));  // __dirname
 
 	// go, go, go!
-	if (duk_pcall(g_duk, 3) != DUK_EXEC_SUCCESS) {
-		// hm, the module threw an error during initialization.  the caller may
+	if (duk_pcall(g_duk, 5) != DUK_EXEC_SUCCESS) {
+		// if a module throws an error during initialization, the game may
 		// want to retry, so we'll remove it from the cache...
 		duk_push_global_stash(g_duk);
 		duk_get_prop_string(g_duk, -1, "moduleCache");
 		duk_del_prop_string(g_duk, -1, filename);
 		duk_pop_2(g_duk);
-		duk_remove(g_duk, -2);  // ...and leave the error on the stack.
+		duk_remove(g_duk, -2);  // ...but leave the error on the stack for now.
 		return DUK_EXEC_ERROR;
 	}
 	duk_pop(g_duk);
 	
-	// set `module.loaded` to true
+	// module executed successfully, set `module.loaded` to true
 	duk_push_true(g_duk);
 	duk_put_prop_string(g_duk, -2, "loaded");
 
 have_module:
-	// `module` should be on the stack, we need `module.exports`
+	// `module` is on the stack, we need `module.exports`
 	duk_get_prop_string(g_duk, -1, "exports");
 	duk_remove(g_duk, -2);
 	return DUK_EXEC_SUCCESS;
 }
 
-path_t*
-cjs_resolve(const char* id, const char* origin, const char* sys_origin)
+static path_t*
+find_module(const char* id, const char* origin, const char* sys_origin)
 {
 	const char* const filenames[] =
 	{
 		"%s",
 		"%s.js",
+		"%s.ts",
+		"%s.coffee",
 		"%s/index.js",
+		"%s/index.ts",
+		"%s/index.coffee",
 	};
-	
+
 	path_t* origin_path;
 	char*   filename;
 	path_t* path;
@@ -137,7 +148,7 @@ cjs_resolve(const char* id, const char* origin, const char* sys_origin)
 	else
 		// resolve module from designated module repository
 		origin_path = path_new(sys_origin);
-	
+
 	for (i = 0; i < (int)(sizeof(filenames) / sizeof(filenames[0])); ++i) {
 		path = path_dup(origin_path);
 		filename = strnewf(filenames[i], id);
@@ -152,8 +163,6 @@ cjs_resolve(const char* id, const char* origin, const char* sys_origin)
 	}
 	return NULL;
 }
-
-
 
 void
 init_commonjs_api(void)
@@ -177,12 +186,12 @@ js_require(duk_context* ctx)
 	caller_id = duk_get_string(ctx, -1);
 	id = duk_require_string(ctx, 0);
 
-	if (!(path = cjs_resolve(id, caller_id, "lib/"))
-		&& !(path = cjs_resolve(id, caller_id, "#/modules/")))
+	if (!(path = find_module(id, caller_id, "lib/"))
+		&& !(path = find_module(id, caller_id, "#/modules/")))
 	{
 		duk_error_ni(g_duk, -1, DUK_ERR_REFERENCE_ERROR, "unable to resolve require `%s`", id);
 	}
-	if (duk_peval_module(path_cstr(path)) != DUK_EXEC_SUCCESS)
+	if (cjs_eval_module(path_cstr(path)) != DUK_EXEC_SUCCESS)
 		duk_throw(ctx);
 	return 1;
 }
