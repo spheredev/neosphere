727bac092906261ad0fa4eeb6954116da8c8a6d4
Bruce Pascoe <fatcerberus@icloud.com>
2016-09-12 03:28:23 -0400

    Add joy module to miniRT
    This provides a simplified joystick API for games to use when they don't
    need the flexibility of the Sphere v2 Joystick API.

---
+++ assets/system/modules/joy.js
@@ -0,0 +1,22 @@
+/**
+ *  miniRT joy CommonJS module
+ *  (c) 2015-2016 Fat Cerberus
+**/
+
+'use strict';
+module.exports =
+{
+	get P1() { return devices[0] || Joystick.Null; },
+	get P2() { return devices[1] || Joystick.Null; },
+	get P3() { return devices[2] || Joystick.Null; },
+	get P4() { return devices[3] || Joystick.Null; },
+};
+
+const link = require('link');
+
+// historically, Sphere requires a gamepad with at least 2 axes (X/Y) and
+// 5 buttons (A, B, X, Y, Start) for full operation.
+var devices = link(Joystick.getDevices())
+	.where(function(it) { return it.numAxes >= 2; })
+	.where(function(it) { return it.numButtons >= 5; })
+	.toArray();
