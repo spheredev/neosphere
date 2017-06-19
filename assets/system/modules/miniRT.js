19387d705d2ef0ca30965a1a57eda222ad8395f4
Bruce Pascoe <fatcerberus@icloud.com>
2017-06-19 02:32:22 -0400

    Add miniRT aggregate module
    This allows for more concise imports when using ES6 modules (the
    recommended module style for Sphere v2).  All needed miniRT objects can
    be pulled in with a single `import` statement this way.

---
+++ assets/system/modules/miniRT.js
@@ -0,0 +1,21 @@
+/***
+ *   miniRT aggregated CommonJS module
+  *  (c) 2015-2017 Fat Cerberus
+***/
+
+'use strict';
+exports.__esModule = true;
+
+Object.assign(exports,
+{
+	assert:   require('assert'),
+	Delegate: require('delegate'),
+	from:     require('from'),
+	Logger:   require('logger'),
+	Music:    require('music'),
+	Prim:     require('prim'),
+	Random:   require('random'),
+	Scene:    require('scene'),
+	Terminal: require('terminal'),
+	Thread:   require('thread'),
+});
