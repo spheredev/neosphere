0c0e1040a1b33aa9cd534ab7aa65039cdca9dc62
Bruce Pascoe <fatcerberus@icloud.com>
2017-09-15 17:47:34 -0400

    Optimize class ID check some more
    Now does a direct integer comparison to check the type of an object,
    only calling into JS to get the host pointer.

--- src/shared/api.c
+++ src/shared/api.c
@@ -8,15 +8,23 @@
 #include "jsal.h"
 #include "vector.h"
 
-struct class_info
+struct class_data
 {
 	js_finalizer_t finalizer;
 	int            id;
 	char*          name;
 };
 
-static int         class_id_from_name (const char* name);
-static const char* class_name_from_id (int class_id);
+struct object_data
+{
+	int            class_id;
+	js_finalizer_t finalizer;
+	void*          ptr;
+};
+
+static void        on_finalize_sphere_object (void* host_ptr);
+static int         class_id_from_name        (const char* name);
+static const char* class_name_from_id        (int class_id);
 
 static vector_t* s_classes;
 static js_ref_t* s_prototypes;
@@ -24,7 +32,7 @@ static js_ref_t* s_prototypes;
 void
 api_init(void)
 {
-	s_classes = vector_new(sizeof(struct class_info));
+	s_classes = vector_new(sizeof(struct class_data));
 	
 	// JavaScript 'global' binding (like Node.js)
 	// also map global 'exports' to the global object, as TypeScript likes to add
@@ -48,15 +56,15 @@ api_init(void)
 void
 api_uninit(void)
 {
-	struct class_info* class_info;
+	struct class_data* class_data;
 	
 	iter_t iter;
 
 	jsal_unref(s_prototypes);
 	iter = vector_enum(s_classes);
 	while (iter_next(&iter)) {
-		class_info = iter.ptr;
-		free(class_info->name);
+		class_data = iter.ptr;
+		free(class_data->name);
 	}
 	vector_free(s_classes);
 }
@@ -105,31 +113,24 @@ api_define_class(const char* name, int class_id, js_function_t constructor, js_f
 	// note: if no constructor function is given, a constructor binding will not be created.
 	//       this is useful for types which can only be created via factory methods.
 
-	struct class_info class_info;
+	struct class_data class_data;
 
-	class_info.id = class_id;
-	class_info.name = strdup(name);
-	class_info.finalizer = finalizer;
-	vector_push(s_classes, &class_info);
+	class_data.id = class_id;
+	class_data.name = strdup(name);
+	class_data.finalizer = finalizer;
+	vector_push(s_classes, &class_data);
 
-	// construct a prototype for the new class, leaving it on the
-	// value stack afterwards.
+	// construct a JS prototype for the new class
 	jsal_push_new_object();
-	jsal_push_eval("({ writable: false, enumerable: false, configurable: false })");
-	jsal_push_int(class_id);
-	jsal_put_prop_string(-2, "value");
-	jsal_def_prop_string(-2, "___iC");
-
-	// save the prototype to the hidden stash.  this ensures it remains accessible
-	// internally even if the constructor is overwritten.
 	jsal_push_ref(s_prototypes);
 	jsal_dup(-2);
 	jsal_put_prop_index(-2, class_id);
 	jsal_pop(1);
 
+	// register a global constructor, if applicable
 	if (constructor != NULL) {
 		jsal_push_constructor(constructor, name, 0, 0);
-		
+
 		jsal_push_new_object();
 		jsal_dup(-3);
 		jsal_put_prop_string(-2, "value");
@@ -281,36 +282,37 @@ api_define_static_prop(const char* namespace_name, const char* name, js_function
 bool
 jsal_is_class_obj(int index, int class_id)
 {
-	bool result;
+	struct object_data* data;
 
 	if (!jsal_is_object(index))
 		return false;
 
-	jsal_get_prop_string(index, "___iC");
-	result = class_id == jsal_get_int(-1);
-	jsal_pop(1);
-	return result;
+	data = jsal_get_host_data(index);
+	return class_id == data->class_id;
 }
 
 int
 jsal_push_class_obj(int class_id, void* udata)
 {
-	struct class_info* class_info;
-	js_finalizer_t     finalizer = NULL;
-	int                index;
+	struct class_data*  class_data;
+	js_finalizer_t      finalizer = NULL;
+	int                 index;
+	struct object_data* object_data;
 	
 	iter_t iter;
 
 	iter = vector_enum(s_classes);
 	while (iter_next(&iter)) {
-		class_info = iter.ptr;
-		if (class_id == class_info->id)
-			finalizer = class_info->finalizer;
+		class_data = iter.ptr;
+		if (class_id == class_data->id)
+			finalizer = class_data->finalizer;
 	}
 	
-	index = jsal_push_new_host_object(udata, NULL);
-	if (finalizer != NULL)
-		jsal_set_finalizer(-1, finalizer);
+	object_data = malloc(sizeof(struct object_data));
+	object_data->class_id = class_id;
+	object_data->finalizer = finalizer;
+	object_data->ptr = udata;
+	index = jsal_push_new_host_object(object_data, on_finalize_sphere_object);
 
 	jsal_push_ref(s_prototypes);
 	jsal_get_prop_index(-1, class_id);
@@ -332,25 +334,42 @@ jsal_push_class_prototype(int class_id)
 void*
 jsal_require_class_obj(int index, int class_id)
 {
-	if (!jsal_is_class_obj(index, class_id)) {
+	struct object_data* data;
+	
+	data = jsal_get_host_data(index);
+	if (data->class_id != class_id) {
 		jsal_dup(index);
 		jsal_push_new_error(JS_TYPE_ERROR, "'%s' is not a %s object", jsal_to_string(-1), class_name_from_id(class_id));
 		jsal_remove(-2);
 		jsal_throw();
 	}
-	return jsal_get_host_data(index);
+	return data->ptr;
 }
 
 void
 jsal_set_class_ptr(int index, void* udata)
 {
-	jsal_set_host_data(index, udata);
+	struct object_data* data;
+
+	data = jsal_get_host_data(index);
+	data->ptr = udata;
+}
+
+static void
+on_finalize_sphere_object(void* host_ptr)
+{
+	struct object_data* object;
+
+	object = host_ptr;
+	if (object->finalizer != NULL) 
+		object->finalizer(object->ptr);
+	free(object);
 }
 
 static int
 class_id_from_name(const char* name)
 {
-	struct class_info* class;
+	struct class_data* class;
 
 	iter_t iter;
 
@@ -366,7 +385,7 @@ class_id_from_name(const char* name)
 static const char*
 class_name_from_id(int class_id)
 {
-	struct class_info* class;
+	struct class_data* class;
 	
 	iter_t iter;
 
