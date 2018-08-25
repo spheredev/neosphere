d7ec5d4a7ceec9e682f98a124374b240aa537cd5
Bruce Pascoe <fatcerberus@icloud.com>
2018-08-25 09:38:40 -0400

    Set version to X.X.X after 5.2.12 release
    Now I'm thinking it's time to begin work on miniSphere 5.3.  Hopefully
    no more crazy bugs turn up...

--- src/plugin/Properties/AssemblyInfo.cs
+++ src/plugin/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.2.12.2891")]
-[assembly: AssemblyFileVersion("5.2.12.2891")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
